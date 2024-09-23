da4ae4e66ecf49be2d97982b7a8f6c5145b4f839
ckashila <ckashila66@gmail.com>
2024-09-23 22:42:31 +0200

    First Commit

--- src/components/FinishScreen.js
+++ src/components/FinishScreen.js
@@ -1,28 +1,28 @@
 function FinishScreen({ points, maxPossiblePoints, highscore, dispatch }) {
-  const percentage = (points / maxPossiblePoints) * 100;
+	const percentage = (points / maxPossiblePoints) * 100;
 
-  let emoji;
-  if (percentage === 100) emoji = "🥇";
-  if (percentage >= 80 && percentage < 100) emoji = "🎉";
-  if (percentage >= 50 && percentage < 80) emoji = "🙃";
-  if (percentage >= 0 && percentage < 50) emoji = "🤨";
-  if (percentage === 0) emoji = "🤦‍♂️";
+	let emoji;
+	if (percentage === 100) emoji = "🥇";
+	if (percentage >= 80 && percentage < 100) emoji = "🎉";
+	if (percentage >= 50 && percentage < 80) emoji = "🙃";
+	if (percentage >= 0 && percentage < 50) emoji = "🤨";
+	if (percentage === 0) emoji = "🤦‍♂️";
 
-  return (
-    <div className="result_container">
-      <p className="result">
-        <span>{emoji}</span> You scored <strong>{points}</strong> out of{" "}
-        {maxPossiblePoints} ({Math.ceil(percentage)}%)
-      </p>
-      <p className="highscore">(Highscore: {highscore} points)</p>
-      <button
-        className="btn btn-ui"
-        onClick={() => dispatch({ type: "restart" })}
-      >
-        Restart quiz
-      </button>
-    </div>
-  );
+	return (
+		<div className="result_container">
+			<p className="result">
+				<span>{emoji}</span> Has tret <strong>{points}</strong> punts de{" "}
+				{maxPossiblePoints} ({Math.ceil(percentage)}%)
+			</p>
+			<p className="highscore">(Puntuació més alta: {highscore} punts)</p>
+			<button
+				className="btn btn-ui"
+				onClick={() => dispatch({ type: "restart" })}
+			>
+				Torna-ho a provar.
+			</button>
+		</div>
+	);
 }
 
 export default FinishScreen;
--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,16 +1,16 @@
 function StartScreen({ numQuestions, dispatch }) {
-  return (
-    <div className="start">
-      <h2>Welcome to The React Quiz!</h2>
-      <h3>{numQuestions} questions to test your React mastery</h3>
-      <button
-        className="btn btn-ui"
-        onClick={() => dispatch({ type: "start" })}
-      >
-        Let's start
-      </button>
-    </div>
-  );
+	return (
+		<div className="start">
+			<h2>Et vas enterar de tot ?</h2>
+			<h3>{numQuestions} preguntes per posar a prova el teu coneixement</h3>
+			<button
+				className="btn btn-ui"
+				onClick={() => dispatch({ type: "start" })}
+			>
+				Comencem!
+			</button>
+		</div>
+	);
 }
 
 export default StartScreen;
